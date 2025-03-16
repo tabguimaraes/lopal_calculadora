8b7b652e8280229179f82cc25d1f56b179150480
Tiago Guimaraes <tab.guimaraes@gmail.com>
2025-03-16 16:31:35 -0300

    funcao trapezio e triangulo ok

--- src/br/sp/senai/jandira/calculadora/Main.java
+++ src/br/sp/senai/jandira/calculadora/Main.java
@@ -3,21 +3,22 @@ package br.sp.senai.jandira.calculadora;
 import br.sp.senai.jandira.calculadora.model.Circulo;
 import br.sp.senai.jandira.calculadora.model.Quadrado;
 import br.sp.senai.jandira.calculadora.model.Retangulo;
+import br.sp.senai.jandira.calculadora.model.Trapezio;
+import br.sp.senai.jandira.calculadora.model.Triangulo;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Circulo circulo = new Circulo();
+		circulo.setRaio(12);
+
+		System.out.println("Área do círculo: " + circulo.calcularArea());
+
 		Quadrado quadrado = new Quadrado();
 		quadrado.setLado(10);
 
 		System.out.println("Área do quadrado: " + quadrado.calcularArea());
 		System.out.println("Perímetro do quadrado: " + quadrado.calcularPerimetro());
-
-		Circulo circulo = new Circulo();
-		circulo.setRaio(12);
-
-		System.out.println("Área do círculo: " + circulo.calcularArea());
 
 		Retangulo retangulo = new Retangulo();
 		retangulo.setBase(10);
@@ -25,6 +26,20 @@ public class Main {
 
 		System.out.println("Área do retângulo: " + retangulo.calcularArea());
 		System.out.println("Perímetro do retângulo: " + retangulo.calcularPerimetro());
+
+		Trapezio trapezio = new Trapezio();
+		trapezio.setBaseMaior(12);
+		trapezio.setBaseMenor(4);
+		trapezio.setAltura(5);
+
+		System.out.println("Área do trapézio: " + trapezio.calcularArea());
+
+		Triangulo triangulo = new Triangulo();
+		triangulo.setBase(10);
+		triangulo.setAltura(5);
+
+		System.out.println("Área do triângulo: " + triangulo.calcularArea());
+
 	}
 
 }
--- src/br/sp/senai/jandira/calculadora/model/Triangulo.java
+++ src/br/sp/senai/jandira/calculadora/model/Triangulo.java
@@ -1,5 +1,27 @@
 package br.sp.senai.jandira.calculadora.model;
 
 public class Triangulo {
+	private double base;
+	private double altura;
+
+	public double getBase() {
+		return base;
+	}
+
+	public void setBase(double base) {
+		this.base = base;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+
+	public double calcularArea() {
+		return (base * altura) / 2;
+	}
 
 }
